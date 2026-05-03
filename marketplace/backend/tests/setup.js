4c41d1caaa9a4a852119111a0ff1863b9c1a7387
superprince1 <67815093+superprince1@users.noreply.github.com>
2026-05-03 11:08:21 +0100

    unzipgit

---
+++ marketplace/backend/tests/setup.js
@@ -0,0 +1,25 @@
+const { MongoMemoryServer } = require('mongodb-memory-server');
+const mongoose = require('mongoose');
+const { app } = require('../server');
+
+let mongoServer;
+
+beforeAll(async () => {
+  mongoServer = await MongoMemoryServer.create();
+  const uri = mongoServer.getUri();
+  await mongoose.connect(uri);
+});
+
+afterAll(async () => {
+  await mongoose.disconnect();
+  await mongoServer.stop();
+});
+
+afterEach(async () => {
+  const collections = mongoose.connection.collections;
+  for (const key in collections) {
+    await collections[key].deleteMany();
+  }
+});
+
+module.exports = { app };
