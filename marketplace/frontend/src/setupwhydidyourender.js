920cd0461320bda863cbeaba9264edf7edbc2ad5
superprince1 <67815093+superprince1@users.noreply.github.com>
2026-05-03 11:06:14 +0100

    unzipgit

---
+++ marketplace/frontend/src/setupwhydidyourender.js
@@ -0,0 +1,11 @@
+// src/setupWhyDidYouRender.js
+import React from 'react';
+
+if (process.env.NODE_ENV === 'development') {
+  const whyDidYouRender = require('@welldone-software/why-did-you-render');
+  whyDidYouRender(React, {
+    trackAllPureComponents: true,
+    trackHooks: true,
+    logOnDifferentValues: true,
+  });
+}
